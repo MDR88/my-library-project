17c5e4ab518678c76d8e5dbebba937ed8fd639cb
mdroberts74 <mikedroberts88@gmail.com>
2018-07-17 11:25:43 -0500

    Making main page with a button to add a book.

--- src/scripts/welcome.js
+++ src/scripts/welcome.js
@@ -1,16 +1,24 @@
+const $ = require("jQuery")
 
 
-const $ = require("jquery")
 //I am creating an object "homePageObject" that will load when landing on the page.
 const homePageObject = Object.create({}, {
-buildHome: {
-value:() => {
-    //This select the main div on the index.html
-    const mainDiv = document.querySelector(".main-div-container");
+    buildHome: {
+    value:() => {
+        //This selects the main div on the index.html
+        const mainDiv = document.querySelector(".main--container");
+        //Creating a main div under
+        const $divMainPage = $("<div>").addClass("div-main");
+        const $paraRef = $("<p>");
 
+        const $h1WelcomeMain = $("<h1>").text("Welcome!");
+        $h1WelcomeMain.attr("id", "h1WelcomeMain");
+        
+        //appends to DOM
+        $divMainPage.appendTo(mainDiv);
+    }
+    }
+    })
 
-
-}
-}
-
-})
+    //exports buildWelcome module
+module.exports = homePageObject
