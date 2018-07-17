0b6fc46d7f8d72688092f096cb8a9bdd8c34ad47
mdroberts74 <mikedroberts88@gmail.com>
2018-07-17 11:00:58 -0500

    I created the structure, running browserfy, created the files I will use for the first part of the project.

---
+++ src/scripts/welcome.js
@@ -0,0 +1,16 @@
+
+
+const $ = require("jquery")
+//I am creating an object "homePageObject" that will load when landing on the page.
+const homePageObject = Object.create({}, {
+buildHome: {
+value:() => {
+    //This select the main div on the index.html
+    const mainDiv = document.querySelector(".main-div-container");
+
+
+
+}
+}
+
+})
